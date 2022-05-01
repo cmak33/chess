61b2c1cede6990abbf8f562dbca845318105246b
hahahalol9 <cmak7543@mail.ru>
2022-05-01 17:17:17 +0300

    point class added

---
+++ src/main/java/com/chess/chessapplication/board/Point.java
@@ -0,0 +1,20 @@
+package com.chess.chessapplication.board;
+
+public class Point {
+    private int y;
+    private int x;
+
+    public Point(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+}
