ad5ba9425936498d008b17823a7865ac696dba5c
hahahalol9 <cmak7543@mail.ru>
2022-05-08 19:00:23 +0300

    move class refactored : main figure movement included in initial movements list

--- src/main/java/com/chess/chessapplication/models/move/Move.java
+++ src/main/java/com/chess/chessapplication/models/move/Move.java
@@ -3,6 +3,8 @@ package com.chess.chessapplication.models.move;
 import com.chess.chessapplication.models.figures.abstract_figure.Figure;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Move {
@@ -12,7 +14,7 @@ public class Move {
 
     public Move(FigureMovement mainFigureMovement,Figure beatenFigure){
         this.mainFigureMovement = mainFigureMovement;
-        movements = new ArrayList<>();
+        movements = new ArrayList<>(Collections.singleton(mainFigureMovement));
         this.beatenFigure = beatenFigure;
     }
 
